fa92983147bae8fd06e99d99dae2d6e70eda328b
rt-inconnect <r.toshpulatov@gmail.com>
2016-09-30 15:43:04 +0500

    add page update

---
+++ backend/views/page/_form.php
@@ -0,0 +1,25 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\Page */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="page-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?php echo $form->field($model, 'createdAt')->textInput(['class' => 'form-control pickadate']) ?>
+
+    <div class="form-group">
+        <?php echo Html::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Save'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
+
+<script type="text/javascript"> $('.pickadate').pickadate(); </script>
